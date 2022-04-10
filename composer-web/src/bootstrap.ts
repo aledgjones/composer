eb1bee83acdab22f2f24a0491a31d4cead377066
Aled Jones <aledjones.viola@gmail.com>
2022-04-10 12:30:30 +0100

    vite

--- composer-web/src/bootstrap.ts
+++ composer-web/src/bootstrap.ts
@@ -1,4 +1,9 @@
+import init from "composer-engine";
 // A dependency graph that contains any wasm must all be imported
 // asynchronously. This `bootstrap.js` file does the single async import, so
 // that no one else needs to worry about it again.
-import("./index").catch((e) => console.error("Error importing `index.ts`:", e));
+init().then(() => {
+  import("./index").catch((e) =>
+    console.error("Error importing `index.ts`:", e)
+  );
+});
